9f02d61ff4bc6a117bec626f3fb8bdeb2a850720
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-22 21:02:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,10 +32,10 @@ def monopoly_with_third_tier
 	      {
 	        pieces: 4,
 	        rent_in_dollars: {
-	          one_piece_owned = {$25},
-	          two_pieces_owned = {$50},
-	          three_pieces_owned = {$100},
-	          four_pieces_owned = {$200}
+	          one_piece_owned = 25,
+	          two_pieces_owned = 50,
+	          three_pieces_owned = 100,
+	          four_pieces_owned = 200
 	           }
 	        names: {
 	          reading_railroad = {},
@@ -59,16 +59,16 @@ def monopoly_with_fourth_tier
 	      {
 	        pieces: 4,
 	        rent_in_dollars: {
-	          one_piece_owned = "$25",
-	          two_pieces_owned = "$50",
-	          three_pieces_owned = "$100",
-	          four_pieces_owned = "$200"
+	          one_piece_owned = 25,
+	          two_pieces_owned = 50,
+	          three_pieces_owned = 100,
+	          four_pieces_owned = 200
 	           }
 	        names: {
-	          reading_railroad = "$100",
-	          pennsylvania_railroad = "$200",
-	          b_and_o_railroad = "$400",
-	          shortline_railroad= "$800"
+	          reading_railroad = 100,
+	          pennsylvania_railroad = 200,
+	          b_and_o_railroad = 400,
+	          shortline_railroad= 800
 	        }
 	      }
 	    }
